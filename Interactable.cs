e42ff7c38d0860746893b981c5cd25ab312b6f05
max-behrens <40147775+max-behrens@users.noreply.github.com>
2020-07-19 23:10:38 +0100

    Add files via upload

---
+++ Interactable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float radius = 3f;
+
+    void OnDrawGizmosSelected ()
+    {
+        OnDrawGizmosSelected.color = Color.yellow;
+        OnDrawGizmosSelected.DrawWireSphere(tranform.position, radius);
+    }
+}
---
+++ ThirdPersonMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+
+    }
+}
